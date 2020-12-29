d732e993a416fff739c0611bf38adf38978756a8
pufflik <pufflik@gmail.com>
2020-12-29 17:56:04 +0200

    Code style

--- template/functions.php
+++ template/functions.php
@@ -18,10 +18,10 @@ remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
  * Theme assets
  */
 add_action('wp_enqueue_scripts', function () {
-    $manifest = json_decode(file_get_contents('build/assets.json', true));
-    $main = $manifest->main;
-    wp_enqueue_style('theme-css', get_template_directory_uri() . "/build/" . $main->css,  false, null);
-    wp_enqueue_script('theme-js', get_template_directory_uri() . "/build/" . $main->js, ['jquery'], null, true);
+		$manifest = json_decode(file_get_contents('build/assets.json', true));
+		$main = $manifest->main;
+		wp_enqueue_style('theme-css', get_template_directory_uri() . "/build/" . $main->css,  false, null);
+		wp_enqueue_script('theme-js', get_template_directory_uri() . "/build/" . $main->js, ['jquery'], null, true);
 }, 100);
 
 
@@ -29,42 +29,42 @@ add_action('wp_enqueue_scripts', function () {
  * Theme setup
  */
 add_action('after_setup_theme', function () {
-    /**
-     * Enable features from Soil when plugin is activated
-     * @link https://roots.io/plugins/soil/
-     */
-    add_theme_support('soil-clean-up');
-    add_theme_support('soil-jquery-cdn');
-    add_theme_support('soil-nav-walker');
-    add_theme_support('soil-nice-search');
-    add_theme_support('soil-relative-urls');
-    /**
-     * Enable plugins to manage the document title
-     * @link https://developer.wordpress.org/reference/functions/add_theme_support/#title-tag
-     */
-    add_theme_support('title-tag');
-    /**
-     * Register navigation menus
-     * @link https://developer.wordpress.org/reference/functions/register_nav_menus/
-     */
-    register_nav_menus([
-        'primary_navigation' => __('Primary Navigation', 'mini')
-    ]);
-    /**
-     * Enable post thumbnails
-     * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-     */
-    add_theme_support('post-thumbnails');
-    /**
-     * Enable HTML5 markup support
-     * @link https://developer.wordpress.org/reference/functions/add_theme_support/#html5
-     */
-    add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
-    /**
-     * Enable selective refresh for widgets in customizer
-     * @link https://developer.wordpress.org/themes/advanced-topics/customizer-api/#theme-support-in-sidebars
-     */
-    // add_theme_support('customize-selective-refresh-widgets');
+		/**
+		 * Enable features from Soil when plugin is activated
+		 * @link https://roots.io/plugins/soil/
+		 */
+		add_theme_support('soil-clean-up');
+		add_theme_support('soil-jquery-cdn');
+		add_theme_support('soil-nav-walker');
+		add_theme_support('soil-nice-search');
+		add_theme_support('soil-relative-urls');
+		/**
+		 * Enable plugins to manage the document title
+		 * @link https://developer.wordpress.org/reference/functions/add_theme_support/#title-tag
+		 */
+		add_theme_support('title-tag');
+		/**
+		 * Register navigation menus
+		 * @link https://developer.wordpress.org/reference/functions/register_nav_menus/
+		 */
+		register_nav_menus([
+				'primary_navigation' => __('Primary Navigation', 'mini')
+		]);
+		/**
+		 * Enable post thumbnails
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support('post-thumbnails');
+		/**
+		 * Enable HTML5 markup support
+		 * @link https://developer.wordpress.org/reference/functions/add_theme_support/#html5
+		 */
+		add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
+		/**
+		 * Enable selective refresh for widgets in customizer
+		 * @link https://developer.wordpress.org/themes/advanced-topics/customizer-api/#theme-support-in-sidebars
+		 */
+		// add_theme_support('customize-selective-refresh-widgets');
 
 
 }, 20);
@@ -86,28 +86,28 @@ add_action('rest_api_init', function () {
 */
 function get_post_for_url($data)
 {
-    $postId    = url_to_postid($data['url']);
-    $postType  = get_post_type($postId);
-    $controller = new WP_REST_Posts_Controller($postType);
-    $request    = new WP_REST_Request('GET', "/wp/v2/{$postType}s/{$postId}");
-    $request->set_url_params(array('id' => $postId));
-    return $controller->get_item($request);
+		$postId    = url_to_postid($data['url']);
+		$postType  = get_post_type($postId);
+		$controller = new WP_REST_Posts_Controller($postType);
+		$request    = new WP_REST_Request('GET', "/wp/v2/{$postType}s/{$postId}");
+		$request->set_url_params(array('id' => $postId));
+		return $controller->get_item($request);
 }
 
 add_filter('body_class', 'add_slug_to_body_class'); // Add slug to body class (Starkers build)
 
 // Add page slug to body class, love this - Credit: Starkers Wordpress Theme
 function add_slug_to_body_class($classes) {
-    global $post;
-    if (is_home()) {
-        $key = array_search('blog', $classes);
-        if ($key > -1) {
-            unset($classes[$key]);
-        }
-    } elseif (is_page()) {
-        $classes[] = sanitize_html_class($post->post_name);
-    } elseif (is_singular()) {
-        $classes[] = sanitize_html_class($post->post_name);
-    }
-    return $classes;
+		global $post;
+		if (is_home()) {
+				$key = array_search('blog', $classes);
+				if ($key > -1) {
+						unset($classes[$key]);
+				}
+		} elseif (is_page()) {
+				$classes[] = sanitize_html_class($post->post_name);
+		} elseif (is_singular()) {
+				$classes[] = sanitize_html_class($post->post_name);
+		}
+		return $classes;
 }
